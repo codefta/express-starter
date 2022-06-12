cf2349d1c17a869be55e4a804e5ab5001108eb42
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-06-12 17:36:30 +0700

    Modify http

--- src/app/http/index.js
+++ src/app/http/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const { unknownEndpoint } = require("./middleware");
+const routes = require("./routes");
 
+/**
+ * HTTP server, put middleware and routes here.
+ *
+ * @param {*} app
+ */
 module.exports = function (app) {
   app.use(express.json());
 
   routes(app);
+
+  app.use(unknownEndpoint);
 };
